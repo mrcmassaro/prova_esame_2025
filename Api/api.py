955c0f87e9399ab8b31859ecf642e72f65aae633
mrcmassaro <167975269+mrcmassaro@users.noreply.github.com>
2025-07-02 23:10:30 +0200

    Modifica api avendo i dati corretti

--- Api/api.py
+++ Api/api.py
@@ -4,8 +4,8 @@ import sqlite3
 import pandas as pd
 
 app = FastAPI(
-    title='Prova api',
-    description='Prima prova fatta per vedere le api su python',
+    title='Pesca api',
+    description='Api dedicate alle consegne a noi assegnate per effettuare delle analisi su dati inerenti alla pesca',
     version='1.0.0',
     contact={
         'name' : 'Marco Massaro',
@@ -14,17 +14,67 @@ app = FastAPI(
 )
 
 def query_db(query: str, params: tuple = ()):
-    conn = sqlite3.connect('prova.db')
+    conn = sqlite3.connect('pesca.db')
     df = pd.read_sql_query(query, conn, params=params)
     conn.close()
     return df
 
-@app.get("/prova_media")
-def get_prova(da_anno: Optional[int] = None, a_anno: Optional[int] = None):
-    query = "SELECT GEO TEXT, TIME_PERIOD, OBS_VALUE, SPECIES FROM prova "
+@app.get("/prod_aree")
+def get_produzione_aree(da_anno: Optional[int] = None, a_anno: Optional[int] = None):
+    query = "SELECT anno, area_geografica, produttivita FROM Produttività_totala_aree "
     params = []
     if da_anno and a_anno:
-        query += "WHERE TIME_PERIOD BETWEEN ? AND ? "
+        query += "WHERE anno BETWEEN ? AND ? "
+        params.extend([da_anno, a_anno])
+    df = query_db(query, tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/prod_totale")
+def get_produzione_nazionale(da_anno: Optional[int] = None, a_anno: Optional[int] = None):
+    query = "SELECT anno, produttivita from Produttività_totale_nazionale "
+    params = []
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? AND ? "
+        params.extend([da_anno, a_anno])
+    df = query_db(query, tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/media_valore_aggiunto")
+def get_media_valore_aggiunto(da_anno: Optional[int] = None, a_anno: Optional[int] = None):
+    query = "SELECT anno, area_geografica, importanza_economica from Media_valore_aggiunto_aree "
+    params = []
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? AND ? "
+        params.extend([da_anno, a_anno])
+    df = query_db(query, tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/media_variazione_occupazione_nazionale")
+def get_produzione_nazionale(da_anno: Optional[int] = None, a_anno: Optional[int] = None):
+    query = "SELECT anno, occupazione from Media_variazione_percentuale_occupazione_nazionale "
+    params = []
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? AND ? "
+        params.extend([da_anno, a_anno])
+    df = query_db(query, tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/media_variazione_occupazione_aree")
+def get_produzione_nazionale(da_anno: Optional[int] = None, a_anno: Optional[int] = None):
+    query = "SELECT anno, occupazione from Media_variazione_percentuale_occupazione_aree "
+    params = []
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? AND ? "
+        params.extend([da_anno, a_anno])
+    df = query_db(query, tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/dati_brutti")
+def get_dati_brutti(da_anno: Optional[int] = None, a_anno: Optional[int] = None, tabella: str = 'economia'):
+    query = f"SELECT * FROM {tabella}"
+    params = []
+    if da_anno and a_anno:
+        query += " WHERE anno BETWEEN ? AND ?"
         params.extend([da_anno, a_anno])
     df = query_db(query, tuple(params))
     return df.to_dict(orient='records')
